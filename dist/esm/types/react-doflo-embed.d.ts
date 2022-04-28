d1b05ea64e2716b185105ff4653989a3fd2a849b
doFlo Admin <dfa@doflo.com>
2022-04-27 20:47:00 -0700

    chore: clean up dist folder

--- dist/esm/types/react-doflo-embed.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export default function DoFloEmbed(props: {
-    src: string;
-    className?: string;
-    scrolling?: boolean;
-}): JSX.Element;
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare function DoFloEmbed(props: {
-    src: string;
-    className?: string;
-    scrolling?: boolean;
-}): JSX.Element;
-
-export { DoFloEmbed as default };
