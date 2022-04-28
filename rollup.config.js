3e6b94e1a2e2b46db8b7a87c8fafdbd204d30b24
doFlo Admin <dfa@doflo.com>
2022-04-27 21:18:26 -0700

    fix: version injector

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,42 @@
-
 const packageJson = require("./package.json");
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-import dts from 'rollup-plugin-dts';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
+import json from "@rollup/plugin-json";
+import versionInjector from "rollup-plugin-version-injector";
 
 export default [
-    {
-        input: "src/react-doflo-embed.tsx",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-            terser(),
-        ],
-        external: ["react", "react-dom", "styled-components"]
-    },
-    {
-        input: "dist/esm/types/react-doflo-embed.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-    },
+  {
+    input: "src/react-doflo-embed.tsx",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      versionInjector(),
+      resolve(),
+      json(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+    external: ["react", "react-dom", "styled-components"],
+  },
+  {
+    input: "dist/esm/types/react-doflo-embed.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
