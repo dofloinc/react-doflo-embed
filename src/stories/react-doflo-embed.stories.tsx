889d4de4e762631c8980871f1cd13da4e1a1f4cf
doFlo Admin <dfa@doflo.com>
2022-04-29 03:06:31 -0700

    chore: storybook support for localhost

--- src/stories/react-doflo-embed.stories.tsx
+++ src/stories/react-doflo-embed.stories.tsx
@@ -1,11 +1,23 @@
-import React from 'react'; //eslint-disable-line no-unused-vars
+import React from "react"; //eslint-disable-line no-unused-vars
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from "@storybook/react";
 
 //import JotformEmbed from '../lib/cjs/react-jotform-embed';
-import DoFloEmbed from '../react-doflo-embed';
+import DoFloEmbed from "../react-doflo-embed";
 
-storiesOf('DoFloEmbed', module)
-.add('Simple', () => <DoFloEmbed src="https://forms.doflo.com/iframe/73244192484358" />)
-.add('Simple Trailing Slash', () => <DoFloEmbed src="https://forms.doflo.com/iframe/73244192484358/" />)
-.add('Scrollable', () => <DoFloEmbed src="https://forms.doflo.com/iframe/73244192484358/" scrolling={true} />);
+storiesOf("DoFloEmbed", module)
+  .add("Simple (localhost)", () => (
+    <DoFloEmbed src="http://localhost:3007/form/73244192d484358" />
+  ))
+  .add("Simple (prod)", () => (
+    <DoFloEmbed src="https://forms.doflo.com/form/73244192d484358" />
+  ))
+  .add("Simple Trailing Slash", () => (
+    <DoFloEmbed src="https://forms.doflo.com/form/73d244192484358/" />
+  ))
+  .add("Scrollable", () => (
+    <DoFloEmbed
+      src="https://forms.doflo.com/form/732441924d84358/"
+      scrolling={true}
+    />
+  ));
